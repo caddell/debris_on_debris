d9f1da4aca387d1dc0c3700b77782afa3a25e919
JD Caddell <caddell.jd@gmail.com>
2021-07-16 14:46:38 -0600

    first commit

--- initial_eda.R
+++ initial_eda.R
@@ -5,3 +5,7 @@ events <- read_csv("DebrisOnDebris_with_names.csv")
 ggplot(events, aes(days_to_tca))+
   geom_histogram(binwidth = .25)+
   theme_minimal()
+
+ggplot(events, aes(pc_best, days_to_tca))+
+  geom_point()+
+  theme_minimal()
