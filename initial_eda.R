f41b986ff0202573b057ac2fb398a6bead0760a1
JD Caddell <caddell.jd@gmail.com>
2021-07-19 13:56:16 -0600

    more eda

--- initial_eda.R
+++ initial_eda.R
@@ -19,14 +19,25 @@ events %>%
   geom_point()+
   theme_minimal()
 
-
-ggplot(events, aes(pc))
+events %>% 
+  filter(pc_nom < .001,
+         event_number < 55) %>%
+  arrange(event_number, desc(days_to_tca)) %>% 
+  ggplot(aes(days_to_tca, pc_nom))+
+  geom_point()+
+  geom_line(aes(group = event_number), arrow = arrow())+
+  scale_x_reverse()+
+  theme_minimal()
 
 event_summary <- events %>% 
   group_by(event_number) %>% 
   summarize(days_tracked = n(), 
             last_notice = min(days_to_tca), 
             first_notice = (max(days_to_tca)),
+            max_pc_best = max(pc_best),
+            min_pc_best = min(pc_best),
+            max_pc_nom = max(pc_nom),
+            min_pc_nom = min(pc_nom),
             lead = first_notice - last_notice) %>% 
   filter(days_tracked < 11) #some weird event history
   
