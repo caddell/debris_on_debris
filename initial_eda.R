0c4cdc8bb6783703918d441a81fe5c85633ed82d
JD Caddell <caddell.jd@gmail.com>
2021-07-19 15:16:29 -0600

    fixed barbell ... again

--- initial_eda.R
+++ initial_eda.R
@@ -20,8 +20,7 @@ events %>%
   theme_minimal()
 
 events %>% 
-  filter(pc_nom < .001,
-         event_number < 55) %>%
+  filter(event_number < 5) %>%
   arrange(event_number, desc(days_to_tca)) %>% 
   ggplot(aes(days_to_tca, pc_nom))+
   geom_point()+
@@ -39,7 +38,7 @@ event_summary <- events %>%
             max_pc_nom = max(pc_nom),
             min_pc_nom = min(pc_nom),
             lead = first_notice - last_notice,
-            single_notice = if_else(first_notice == last_notice, "Single Notice", "Multiple-Notices")) %>% 
+            single_notice = as.factor(if_else(first_notice == last_notice, "Single Notice", "Multiple-Notices"))) %>% 
   filter(days_tracked < 11) #some weird event history
   
 ggplot(event_summary, aes(x = days_tracked))+
@@ -63,17 +62,21 @@ event_summary %>%
   count()
 
 event_summary %>% 
-  filter(last_notice < 1.5) %>% #pull out events that went away
   arrange(desc(first_notice,last_notice)) %>% 
   mutate(order = row_number()) %>% 
-  pivot_longer(cols = c(last_notice, first_notice), names_to = "notice", values_to = "days") %>% 
+  pivot_longer(cols = c(last_notice, first_notice), names_to = "notice", values_to = "days") %>%
+  select(days, order, notice, single_notice) %>% 
+  drop_na() %>% 
   ggplot(aes(days, order))+
-  geom_line(aes(group = event_number), color = 'lightgrey')+
+  geom_line(aes(group = order), color = 'lightgrey')+
   geom_point(aes(color = notice), size = 1.5)+
   scale_color_colorblind()+
   scale_x_continuous(breaks = seq(from = 0 , to = 10, by = 1))+
   theme_minimal()+
+  theme(legend.position = "bottom")+
   labs(y = "Event",
        x = "Days",
        color = "Notice")+
-  facet_wrap(~single_notice, ncol = 1, scales = 'free_y')
+  facet_grid(rows = vars(single_notice))
+
+table(event_summary$single_notice)
